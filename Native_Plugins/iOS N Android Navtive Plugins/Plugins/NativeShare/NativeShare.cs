08cb1b3d9164d3fae97d619235294de80dfd6ab6
Atul Kumar Singh <atulsingh26@live.com>
2020-06-26 15:10:22 +0530

    Native share plugin for iOS added
    Native OS share for ios has beed added also have 3 callbacks sharecomplete , sharecancel and shared with which app . like if user choose facebook for share then will get the callback telling that user had used facebook for sharing

---
+++ Native_Plugins/iOS N Android Navtive Plugins/Plugins/NativeShare/NativeShare.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+public enum SharedActivityType
+{
+	OTHERS,
+	FACEBOOK,
+	TWITTER,
+	INSTAGRAM,
+	CANCEL
+};
+
+public class NativeShare
+{
+	private delegate void shareResultActivityCallBack(bool shareComplete, string shareActivity);
+
+#if !UNITY_EDITOR && UNITY_IOS
+	[System.Runtime.InteropServices.DllImport( "__Internal" )]
+	private static extern void IOSNative_Share( string file, string caption, shareResultActivityCallBack shareActivityCallback);
+#endif
+
+	private string caption;
+
+	private string file;
+
+	public NativeShare()
+	{
+		caption = string.Empty;
+
+		file = string.Empty;
+	}
+
+	public NativeShare SetCaption(string specialMsg = null , string hashtags = null, string urlLink = null)
+	{
+		if (caption != null)
+        {
+            if(!string.IsNullOrEmpty(specialMsg) && !string.IsNullOrWhiteSpace(specialMsg))
+			    this.caption += specialMsg + "\n";
+			if (!string.IsNullOrEmpty(hashtags) && !string.IsNullOrWhiteSpace(hashtags))
+				this.caption += hashtags + "\n";
+			if (!string.IsNullOrEmpty(urlLink) && !string.IsNullOrWhiteSpace(urlLink))
+				this.caption += urlLink + "\n";
+		}
+
+		return this;
+	}
+
+	public NativeShare AddFile(string filePath)
+	{
+		if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+		{
+			file = filePath;
+		}
+		else
+			Debug.LogError("!!!! File does not exist at path or permission denied: " + filePath);
+
+		return this;
+	}
+
+	static System.Action onShareCompleteAction;
+	static System.Action onShareCancelAction;
+	static System.Action<SharedActivityType> onShareActivityAction;
+
+	static bool isSharing;
+
+	[AOT.MonoPInvokeCallback(typeof(shareResultActivityCallBack))]
+	public static void shareResultCallBack(bool shareComplete ,string shareActivity)
+	{
+		isSharing = false;
+
+		Debug.Log("shareResultCallBack called with shareComplete : " + shareComplete + "\n shareActivity : " + shareActivity);
+
+		SharedActivityType sharedActivityType = ReturnSharedActivity(shareActivity);
+
+		if (shareComplete)
+		{
+			onShareCompleteAction?.Invoke();
+		}
+		else if (!shareComplete && sharedActivityType == SharedActivityType.CANCEL)
+		{
+			onShareCancelAction?.Invoke();
+		}
+		onShareActivityAction?.Invoke(sharedActivityType);
+		
+		
+	}
+
+    private static SharedActivityType ReturnSharedActivity(string activtyString)
+    {
+		SharedActivityType sharedActivityType = SharedActivityType.OTHERS;
+#if UNITY_EDITOR
+		sharedActivityType = SharedActivityType.OTHERS;
+#elif UNITY_IOS
+       if (string.IsNullOrEmpty(activtyString) || string.IsNullOrWhiteSpace(activtyString))
+		{
+			sharedActivityType = SharedActivityType.CANCEL;
+		}
+		else if (activtyString.Equals("com.apple.UIKit.activity.PostToFacebook"))
+		{
+			sharedActivityType = SharedActivityType.FACEBOOK;
+		}
+		else if (activtyString.Equals("com.apple.UIKit.activity.PostToTwitter"))
+		{
+			sharedActivityType = SharedActivityType.TWITTER;
+		}
+		else if (activtyString.Equals("com.burbn.instagram.shareextension"))
+		{
+			sharedActivityType = SharedActivityType.INSTAGRAM;
+		}
+		else
+		{
+			sharedActivityType = SharedActivityType.OTHERS;
+		} 
+#endif
+		return sharedActivityType;
+	}
+
+	public void Share(System.Action shareComplete, System.Action shareCancel, System.Action<SharedActivityType> activitySharedWith)
+	{
+		Debug.Log("!!!! Share is called");
+        if (isSharing)
+        {
+			Debug.LogError("!!!! Already sharing going on can not share now aborting !!!!");
+			return;
+        }
+		if (file.Length == 0 && caption.Length == 0)
+		{
+			Debug.LogWarning("!!!! Share Error: attempting to share nothing!");
+			return;
+		}
+
+		onShareCompleteAction = shareComplete;
+		onShareCancelAction = shareCancel;
+		onShareActivityAction = activitySharedWith;
+
+		isSharing = true;
+
+#if UNITY_EDITOR
+		Debug.Log("Shared On Unity Editor !!!");
+		shareResultCallBack(true,"others");
+#elif UNITY_IOS
+		IOSNative_Share( file, caption, shareResultCallBack);
+#else
+		Debug.Log( "No sharing set up for this platform." );
+#endif
+	}
+}
---
+++ Native_Plugins/iOS N Android Navtive Plugins/Plugins/NativeShare/iOS/NativeShare.mm
@@ -0,0 +1,103 @@
+﻿#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef void (*RESULT_CALLBACK)(BOOL,const char*);
+
+@interface NativeShare : NSObject
+{
+    RESULT_CALLBACK shareResultCallBack;
+    UIPopoverController *popover;
+}
+
+@end
+
+@implementation NativeShare
+
+static NativeShare *_sharedInstance;
+
+
++(NativeShare*) sharedInstance
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"Creating NativeShare Shared Instance !!!");
+        _sharedInstance = [[NativeShare alloc]init];
+    });
+    return _sharedInstance;
+}
+
+
+-(id)init
+{
+    self = [super init];
+    
+    if(self)
+    {
+        [self initHelper];
+    }
+    return self;
+}
+
+-(void)initHelper
+{
+    NSLog(@"initHelper called $$$");
+}
+
+-(void)_NativeShare_Share:(const char*)file caption:(const char*) caption_in resultCallBack:(RESULT_CALLBACK) resultCallBack
+{
+    NSMutableArray *shareableItems = [NSMutableArray new];
+        
+    NSString *filePath = [NSString stringWithUTF8String:file];
+    if( filePath != nil )
+    {
+        [shareableItems addObject:[NSURL fileURLWithPath:filePath]];
+    }
+    else
+    {
+        NSLog(@"filePath is null $$$$");
+    }
+    
+    if(caption_in != nil && strlen( caption_in ) > 0 )
+    {
+        [shareableItems addObject:[NSString stringWithUTF8String:caption_in]];
+    }
+    
+    shareResultCallBack = resultCallBack;
+
+    UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:shareableItems  applicationActivities:nil];
+
+    activity.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError ){
+        NSLog(@"Activity %@ Completed : %d",activityType,completed);
+        if(shareResultCallBack != nil)
+        {
+            shareResultCallBack(completed,[activityType UTF8String]);
+        }else
+        {
+            NSLog(@"shareResultCallBack is null $$$$");
+        }
+    };
+    
+    UIViewController *rootViewController = UnityGetGLViewController();
+    
+    if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) // iPhone
+    {
+        [rootViewController presentViewController:activity animated:YES completion:^{NSLog(@"share shown $$$$");}];
+    }
+    else // iPad
+    {
+        popover = [[UIPopoverController alloc] initWithContentViewController:activity];
+        [popover presentPopoverFromRect:CGRectMake( rootViewController.view.frame.size.width / 2, rootViewController.view.frame.size.height / 2, 1, 1 ) inView:rootViewController.view permittedArrowDirections:0 animated:YES];
+    }
+}
+
+@end
+
+extern "C"
+{
+
+void IOSNativeShare( const char* file, const char* caption, RESULT_CALLBACK resultCallBack)
+{
+    [[NativeShare sharedInstance] _NativeShare_Share:file caption:caption resultCallBack:resultCallBack];
+}
+   
+}
